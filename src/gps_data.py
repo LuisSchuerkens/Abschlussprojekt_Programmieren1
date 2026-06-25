21b6fb8bdfe5581b0604492a3414bcb7dd44012f
Luis Schuerkens <sl2030@mci4me.at>
2026-06-25 13:40:09 +0200

    feat: Laden der Daten der CSV Datei

--- src/gps_data.py
+++ src/gps_data.py
@@ -1,5 +1,31 @@
-def load_gps_data(file_path):
+from pathlib import Path
+import pandas as pd
+
+
+def load_gps_data(file_path: str) -> pd.DataFrame:
     """
-    Lädt später die GPS-Daten aus einer Datei.
+    Lädt GPS-Daten aus CSV-Datei.
+
+    Parameter
+    ----------
+    file_path : str
+        Pfad zur CSV-Datei.
+
+    Returns
+    -------
+    pd.DataFrame
+        Eingelesene GPS-Daten.
     """
-    print(f"GPS-Daten werden geladen aus: {file_path}")
+    path = Path(file_path)
+
+    if not path.exists():
+        raise FileNotFoundError(f"Die Datei wurde nicht gefunden: {file_path}")
+
+    gps_data = pd.read_csv(path)
+
+    print("GPS-Daten wurden erfolgreich geladen.")
+    print(f"Anzahl der Datenpunkte: {len(gps_data)}")
+    print("Vorhandene Spalten:")
+    print(gps_data.columns.tolist())
+
+    return gps_data
--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from gps_data import load_gps_data
 
 def main():
-    """später steht hier mehr code; Hauptfunktion des Programms"""
-    file_path = "data/final_project_input_data.csv"
-    load_gps_data(file_path)
+    """Hauptfunktion des Programms."""
     print("E-Bike Simulation gestartet.")
+
+    file_path = "data/final_project_input_data.csv"
+    gps_data = load_gps_data(file_path)
+
+    """für Kontrolle, ob die Daten eingelesen wurden"""
+    print("Erste fünf Zeilen der GPS-Daten:")
+    print(gps_data.head())
+
 
 if __name__ == "__main__":
     main()
