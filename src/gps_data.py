e1fbb9f3cd09060b5da82b28fb24c9c3a9622d40
Luis Schuerkens <sl2030@mci4me.at>
2026-06-25 11:51:28 +0200

    feat: added gps loading module

---
+++ src/gps_data.py
@@ -0,0 +1,5 @@
+def load_gps_data(file_path):
+    """
+    Lädt später die GPS-Daten aus einer Datei.
+    """
+    print(f"GPS-Daten werden geladen aus: {file_path}")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from gps_data import load_gps_data
+
 def main():
     """später steht hier mehr code; Hauptfunktion des Programms"""
+    file_path = "data/final_project_input_data.csv"
+    load_gps_data(file_path)
     print("E-Bike Simulation gestartet.")
 
 if __name__ == "__main__":
